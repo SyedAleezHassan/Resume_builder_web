a1583e5193c01e6c5852f46127350b2ae9a45bd9
SyedAleezHassan <syedaleezhassan@gmail.com>
2024-09-08 02:00:16 +0500

    ..

--- script.ts
+++ script.ts
@@ -34,19 +34,3 @@ form.addEventListener('submit', function (event: Event) {
 
 
 
-
-// // alert('test');
-//  var emailLink = document.getElementById('email-link');
-
-// if (emailLink) {
-//     emailLink.addEventListener('click', function (event) {
-//         event.preventDefault();  
-//         const email = 'syedaleezhassan@gmail.com';
-//         const subject = 'Hello Syed Aleez Hassan'; 
-//         const body = 'Dear Syed,\n\nI would like to get in touch with you.';  // Optional: Pre-fill the body
-        
-//         const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-        
-//         window.location.href = mailtoLink;
-//     });
-// }
