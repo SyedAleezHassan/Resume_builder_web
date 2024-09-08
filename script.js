ab32fc699cbbf7deeb2419add749851fdb92dc36
SyedAleezHassan <syedaleezhassan@gmail.com>
2024-09-08 12:27:46 +0500

    resume buider done

--- script.js
+++ script.js
@@ -23,15 +23,3 @@ form.addEventListener('submit', function (event) {
     // Optionally, hide the form after submission
     document.getElementById('cv-form').style.display = 'none';
 });
-// // alert('test');
-//  var emailLink = document.getElementById('email-link');
-// if (emailLink) {
-//     emailLink.addEventListener('click', function (event) {
-//         event.preventDefault();  
-//         const email = 'syedaleezhassan@gmail.com';
-//         const subject = 'Hello Syed Aleez Hassan'; 
-//         const body = 'Dear Syed,\n\nI would like to get in touch with you.';  // Optional: Pre-fill the body
-//         const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-//         window.location.href = mailtoLink;
-//     });
-// }
